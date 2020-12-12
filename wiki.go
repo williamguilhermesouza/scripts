90cedd9280be497cd4397cc552a721f82db486c0
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-12-12 16:16:47 -0300

    go example

--- wiki.go
+++ wiki.go
@@ -25,7 +25,7 @@ func loadPage(title string) (*Page, error) {
 }
 
 func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page")
+    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page")}
     p1.save()
     p2, _ := loadPage("TestPage")
     fmt.Println(string(p2.Body))
